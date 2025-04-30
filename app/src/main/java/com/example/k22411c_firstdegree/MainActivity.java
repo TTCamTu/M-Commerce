158ac6b677261c52f3cb5bb0ec7a1ed0f1b77e5f
CamTu <tuttc22411c@st.uel.edu.vn>
2025-04-30 22:07:22 +0700

    change_languages_buttons

--- app/src/main/java/com/example/k22411c_firstdegree/MainActivity.java
+++ app/src/main/java/com/example/k22411c_firstdegree/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.k22411c_firstdegree;
-
-import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -35,31 +30,31 @@ public class MainActivity extends AppCompatActivity {
     Button btnNext;
     Button btnExit;
     TextView txtChooseLanguage;
-    Spinner spinnerLanguage;
-    ArrayAdapter<String> adapterLanguage;
+
+    Button btnEnglish;
+    Button btnVietnamese;
+    Button btnKorean;
+    Button btnFrench;
+
     boolean isLoadViewFinished=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
         addControls();
-
         addEvents();
-
-//        addViews();
-
+        isLoadViewFinished = true;
+        updateLanguage(getResources());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    private void UpdateLanguage(Resources resources)
-    {
-        if(isLoadViewFinished==false)return;
+    private void updateLanguage(Resources resources) {
+        if (!isLoadViewFinished) return;
         txtChooseLanguage.setText(resources.getString(R.string.strChooseLanguage));
         edtCoefficientA.setHint(resources.getString(R.string.title_coefficient_a));
         edtCoefficientB.setHint(resources.getString(R.string.title_coefficient_b));
@@ -72,48 +67,31 @@ public class MainActivity extends AppCompatActivity {
         titleResult.setText(resources.getString(R.string.title_result));
         title_app.setText(resources.getString(R.string.title_app));
         setTitle(resources.getString(R.string.app_name));
-        adapterLanguage.clear();
-        adapterLanguage.addAll(resources.getStringArray(R.array.arrLanguage));
-        adapterLanguage.notifyDataSetChanged();
     }
+
+    private void changeLanguage(String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+        updateLanguage(getResources());
+        recreate();
+    }
+
     private void addEvents() {
-        spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-          @Override
-          public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-              String lang="en";
-              if(i==0)
-              {
-                  lang="en";
-              }
-              else if(i==1)
-              {
-                  lang="vi";
-              }
-              else if(i==2)
-              {
-                  lang="ko";
-              }
-              else if(i==3)
-                  {
-                  lang="fr";
-              }
-              Locale locale = new Locale(lang);
-              Locale.setDefault(locale);
-              Configuration configuration = getResources().getConfiguration();
-              configuration.setLocale(locale);
-              Context context= createConfigurationContext(configuration);
-              Resources resources = context.getResources();
-              UpdateLanguage(resources);
-          }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
-        });
+        btnEnglish.setOnClickListener(v -> changeLanguage("en"));
+        btnVietnamese.setOnClickListener(v -> changeLanguage("vi"));
+        btnKorean.setOnClickListener(v -> changeLanguage("ko"));
+        btnFrench.setOnClickListener(v -> changeLanguage("fr"));
+
+        btnSolution.setOnClickListener(this::do_solution);
+        btnNext.setOnClickListener(this::do_next);
+        btnExit.setOnClickListener(this::do_exit);
     }
 
     private void addControls() {
-        txtChooseLanguage= (TextView)
-                findViewById(R.id.txtChooseLanguage);
+        txtChooseLanguage = findViewById(R.id.txtChooseLanguage);
         edtCoefficientA = findViewById(R.id.edtCoefficientA);
         edtCoefficientB = findViewById(R.id.edtCoefficientB);
         txtResult = findViewById(R.id.txtResult);
@@ -124,22 +102,11 @@ public class MainActivity extends AppCompatActivity {
         btnSolution = findViewById(R.id.btnSolution);
         btnNext = findViewById(R.id.btnNext);
         btnExit = findViewById(R.id.btnExit);
-        spinnerLanguage= (Spinner)
-                findViewById(R.id.spinnerLanguage);
-        adapterLanguage=new
-                ArrayAdapter<String>(this,android.R.layout.simple_spinner_item
-        );
-        adapterLanguage.addAll(getResources().getStringArray(R.array.arrLanguage));
-        adapterLanguage.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerLanguage.setAdapter(adapterLanguage);
-        isLoadViewFinished=true;
+        btnEnglish = findViewById(R.id.btnEnglish);
+        btnVietnamese = findViewById(R.id.btnVietnamese);
+        btnKorean = findViewById(R.id.btnKorean);
+        btnFrench = findViewById(R.id.btnFrench);
     }
-
-//    private void addViews() {
-//        edtCoefficientA = findViewById(R.id.edtCoefficientA);
-//        edtCoefficientB = findViewById(R.id.edtCoefficientB);
-//        txtResult = findViewById(R.id.txtResult);
-//    }
 
     public void do_solution(View view) {
 
